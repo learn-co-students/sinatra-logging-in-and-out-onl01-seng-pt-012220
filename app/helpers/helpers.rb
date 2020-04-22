30b18d246acd9f22c21af82a33fbac5414f27944
Charles Pelton <charliejp0311@gmail.com>
2020-04-22 17:25:23 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  def current_user(sessions_hash)
+    binding.pry
+    User.find_by_id(sessions_hash[:user_id])
+  end
+  def is_logged_in?(sessions_hash)
+    # binding.pry
+    !!sessions_hash[:user_id]
+  end
 end
