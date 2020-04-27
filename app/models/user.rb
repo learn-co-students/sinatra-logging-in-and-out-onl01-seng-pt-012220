548e3b42f33e90ae5fef00f3f776c88eab67c481
haileybongo <haileyebongo@gmail.com>
2020-04-27 09:07:53 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
-
+class User < ActiveRecord::Base
+  
+    attr_accessor :username, :password, :balance 
+    
+    @@all = []
+    
+    def initialize (username, password, balance)
+      @username = username
+      @password = password
+      @balance = balance
+      @@all << self 
+    end
+  end
+  
+  
 
